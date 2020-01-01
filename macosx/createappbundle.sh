a827d2c9346476beaf1eb8a969e7d53460674e7d
Nuntipat narkthong <nuntipat@gmail.com>
2020-01-01 17:23:31 +0700

    Update macOS build script to include notarization for compatibility with macOS Catalina

--- macosx/createappbundle.sh
+++ macosx/createappbundle.sh
@@ -8,8 +8,8 @@ fi
 # install packages (original download url: http://s.sudre.free.fr/Software/files/Packages.dmg)
 curl -O https://makerplayground.blob.core.windows.net/tool/macos/Packages.dmg
 hdiutil attach Packages.dmg
-sudo installer -pkg /Volumes/Packages\ 1.2.5/Install\ Packages.pkg -target "/"
-hdiutil detach /Volumes/Packages\ 1.2.5
+sudo installer -pkg /Volumes/Packages\ 1.2.7/Install\ Packages.pkg -target "/"
+hdiutil detach /Volumes/Packages\ 1.2.7
 rm Packages.dmg
 
 # install platformio
@@ -22,7 +22,7 @@ cd ../
 chmod -R 777 platformio
 
 # signed the app bundle
-codesign --deep --force --verbose --sign "Developer ID Application: INGARAGE ASSISTIVE TECHNOLOGY COMPANY LIMITED" Maker\ Playground.app
+codesign --deep --force --timestamp --verbose --sign "Developer ID Application: INGARAGE ASSISTIVE TECHNOLOGY COMPANY LIMITED" Maker\ Playground.app
 
 # verify the app bundle
 codesign --verify -vvvv Maker\ Playground.app
@@ -38,4 +38,29 @@ packagesbuild MakerPlayground_Installer.pkgproj
 cd build
 productsign --sign "Developer ID Installer: INGARAGE ASSISTIVE TECHNOLOGY COMPANY LIMITED (8YJDH97D29)" MakerPlayground-$1.pkg MakerPlayground-$1-signed.pkg
 mv MakerPlayground-$1-signed.pkg MakerPlayground-$1.pkg
+
+# notarization the installer
+echo "Get notarization request uuid..."
+notarize_req_uid=$(xcrun altool --notarize-app \
+    -f MakerPlayground-$1.pkg \
+    --primary-bundle-id io.makerplayground \
+    --username palmnuntipat@hotmail.com --password @env:APPLE_ID_PASSWORD \
+    | grep RequestUUID | awk '{print $3}')
+echo "RequestUUID = $notarize_req_uid"
+
+echo "Poll the server every minute until success..."
+while :
+do
+    notarize_status=$(xcrun altool --notarization-info "$notarize_req_uid" \
+        -u palmnuntipat@hotmail.com --password @env:APPLE_ID_PASSWORD)
+    echo "$notarize_status"
+    if [[ "$notarize_status" == *"Status: success"* ]]; then
+        break
+    fi
+    sleep 60
+done
+
+echo "Staple notarization ticket to the application installer"
+xcrun stapler staple MakerPlayground-$1.pkg
+
 cd ../
