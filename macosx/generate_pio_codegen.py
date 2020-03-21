4aac6d2a55e801f965f547537f9cf1edc51868c9
Nuntipat Narkthong <nuntipat@gmail.com>
2020-03-22 00:24:46 +0700

    Fix notarization issue on macOS

---
+++ macosx/generate_pio_codegen.py
@@ -0,0 +1,18 @@
+import json
+import sys
+import urllib.request
+
+if len(sys.argv) != 2:
+    print ('Usage: python generate_pio_codesign.py <url from Apple Notarization Server>')
+
+response = urllib.request.urlopen(sys.argv[1])
+
+path = []
+errors = json.loads(response.read())
+for error in errors['issues']:
+    if error['path'] not in path and 'platformio/' in error['path']:
+        path.append(error['path'])
+
+path = ['platformio/' + p.split('platformio/')[1] for p in path]
+for p in path:
+    print ('codesign --deep --force --timestamp --options runtime --verbose --sign "Developer ID Application: INGARAGE ASSISTIVE TECHNOLOGY COMPANY LIMITED" ' + p)
