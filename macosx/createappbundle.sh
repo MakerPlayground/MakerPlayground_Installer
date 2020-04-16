e2b350c2f06d33066ec6ccce4dfdf11eec10d95d
Nuntipat Narkthong <nuntipat@gmail.com>
2020-04-16 22:32:30 +0700

    Staple notarization ticket to the application bundle to allow releasing both .app and .pkg package

--- macosx/createappbundle.sh
+++ macosx/createappbundle.sh
@@ -271,7 +271,10 @@ do
     fi
 done
 
+echo "Staple notarization ticket to the application bundle"
+xcrun stapler staple -v Maker\ Playground.app
+
 echo "Staple notarization ticket to the application installer"
-xcrun stapler staple MakerPlayground-$1.pkg
+xcrun stapler staple -v MakerPlayground-$1.pkg
 
 cd ../
